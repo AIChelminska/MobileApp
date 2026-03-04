d4b37e94b795e1da0f6875f1b0c6ac37a2100fab
Aleksandra Chełmińska <aichelminska99@gmail.com>
2026-03-04 01:15:23 +0100

    Components with properties

--- SolutionOrdersMobile/App.tsx
+++ SolutionOrdersMobile/App.tsx
@@ -1,44 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { Text, StyleSheet, ScrollView } from 'react-native';
+import Greeting from './src/components/Greeting';
+import {SafeAreaView } from 'react-native-safe-area-context';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+function App(): React.JSX.Element {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+      <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.content}>
+          <Text style={styles.title}>Hello React Native!</Text>
+          <Text style={styles.subtitle}>with TypeScript 🚀</Text>
+          <Greeting name="Anna" age={25} />
+          <Greeting name="Piotr" isVip={true} />
+          <Greeting name="Kasia" age={30} isVip={true} />
+          <Greeting name="Jan" />
+        </ScrollView>
+      </SafeAreaView>
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  content: {
+    alignItems: 'center',
+    paddingVertical: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    marginTop: 8,
+    marginBottom: 16,
   },
 });
 
